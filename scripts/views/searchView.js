0578784b6713c3410c067cfd1b8def43e69f702a
Michael Bishop <polishthebishop@gmail.com>
2016-09-01 23:02:19 -0700

    Hamburger icon added and menu is functioning

--- scripts/views/searchView.js
+++ scripts/views/searchView.js
@@ -1,18 +1,4 @@
-$('.icon-menu').on('click',function() {
+$('.icon-menu-outline').on('click',function() {
   $('.main-nav ul').toggle('slow');
 
 });
-$(window).on('resize', function() {
-  if ($(window).width() >= 680) {
-    $('.main-nav ul').show();
-  } else {
-    $('.main-nav ul').hide();
-    $('.icon-menu').show();
-  }
-});
-// $('.main-nav').on('click', '.tab', function(e) {
-//   $('.tab-content').hide();
-//   $('#' + $(this).data('content')).fadeIn();
-// });
-//
-// $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
