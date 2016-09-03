dc6c8b423976bb9a173aa73ae4d47c8fa7f8215f
Kyle Winckler <kyle.winckler@gmail.com>
2016-09-03 12:19:38 -0700

    merge

--- server.js
+++ server.js
@@ -6,25 +6,104 @@ var requestProxy = require('express-request-proxy'),
 
 pg.defaults.ssl = true;
 
-app.get('/db', function(req, res) {
-  var db_url = process.env.DATABASE_URL;
+function create (user, callback) {
+  //this example uses the "pg" library
+  //more info here: https://github.com/brianc/node-postgres
+  var conString = process.env.DATABASE_URL;
 
-  var client = new pg.Client(db_url);
+  postgres(conString, function (err, client, done) {
+    if (err) {
+      console.log('could not connect to postgres db', err);
+      return callback(err);
+    }
 
-  client.connect(function (err) {
-    if (err) throw err;
+    var hashedPassword = bcrypt.hashSync(user.password, 10);
 
-    client.query('Select * FROM text_table', function(err, result) {
-      if (err) throw err;
+    var query = 'INSERT INTO users(email, password) VALUES ($1, $2)';
 
-      console.log(res);
-      client.end(function(err) {
-        if (err) throw err;
-      });
-      res.send(result.rows);
+    client.query(query, [user.email, hashedPassword], function (err, result) {
+      // NOTE: always call `done()` here to close
+      // the connection to the database
+      done();
+
+      if (err) {
+        console.log('error executing query', err);
+        return callback(err);
+      }
+
+      if (result.rows.length === 0) {
+        return callback();
+      }
+
+      callback(null);
     });
   });
-});
+}
+
+function login (email, password, callback) {
+  //this example uses the "pg" library
+  //more info here: https://github.com/brianc/node-postgres
+
+  var conString = process.env.DATABASE_URL;
+  postgres(conString, function (err, client, done) {
+    if (err) {
+      console.log('could not connect to postgres db', err);
+      return callback(err);
+    }
+
+    var query = 'SELECT id, nickname, email, password ' +
+                'FROM users WHERE email = $1';
+
+    client.query(query, [email], function (err, result) {
+      // NOTE: always call `done()` here to close
+      // the connection to the database
+      done();
+
+      if (err) {
+        console.log('error executing query', err);
+        return callback(err);
+      }
+
+      if (result.rows.length === 0) {
+        return callback();
+      }
+
+      var user = result.rows[0];
+
+      if (!bcrypt.compareSync(password, user.password)) {
+        return callback();
+      }
+
+      callback(null, {
+        id:         user.id,
+        nickname:   user.nickname,
+        email:      user.email
+      });
+
+
+    });
+  });
+}
+
+// app.get('/db', function(req, res) {
+//   var db_url = process.env.DATABASE_URL;
+//
+//   var client = new pg.Client(db_url);
+//
+//   client.connect(function (err) {
+//     if (err) throw err;
+//
+//     client.query('Select * FROM users', function(err, result) {
+//       if (err) throw err;
+//
+//       console.log(res);
+//       client.end(function(err) {
+//         if (err) throw err;
+//       });
+//       res.send(result.rows);
+//     });
+//   });
+// });
 
 app.get('/vehicle/*', function(request, response) {
   console.log('Routing Edmunds API request');
