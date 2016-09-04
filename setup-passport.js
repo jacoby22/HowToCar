6aa9235307ffdea731296dbbc258268f43dab710
Kyle Winckler <kyle.winckler@gmail.com>
2016-09-04 10:45:42 -0700

    still clogged

--- setup-passport.js
+++ setup-passport.js
@@ -4,7 +4,7 @@ var Auth0Strategy = require('passport-auth0');
 var strategy = new Auth0Strategy({
   domain:       process.env.AUTH0_DOMAIN,
   clientID:     process.env.AUTH0_CLIENT_ID,
-  clientSecret: process.env.AUTHO_CLIENT_SECRET,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
   callbackURL:  '/callback'
 }, function(accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
