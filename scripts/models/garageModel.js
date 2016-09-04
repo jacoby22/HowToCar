007ffd2ecffb59d02de73c5af8d30a4f37c35192
Michael Bishop <polishthebishop@gmail.com>
2016-09-04 12:32:37 -0700

    Template showing in garage, needs work

--- scripts/models/garageModel.js
+++ scripts/models/garageModel.js
@@ -5,5 +5,31 @@
   garage.savedCarMaintenance = [];
   garage.savedCars = [];
 
+  var renderMaintenace = function(carData) {
+    // console.log(carData);
+    var garageTemplateMaintenance = Handlebars.compile($('#maintenance-template').html());
+    // console.log(garageTemplateMaintenance)
+    return garageTemplateMaintenance(carData);
+  };
+  var renderCar = function(carData) {
+    var garageTemplate = Handlebars.compile($('#car-template').html());
+    // console.log(carData);
+    return garageTemplate(carData);
+  };
+//TODO
+  garage.showCar = function() {
+    console.log(garage.savedCars[0]);
+    console.log(garage.savedCarMaintenance[0]);
+    var listMaintenance = renderMaintenace(garage.savedCarMaintenance[0,1,2,3,4,5,6]);
+    var listItem = renderCar(garage.savedCars[0]);
+    $('#car').append(listItem);
+    $('#maintenance-list').append(listMaintenance);
+  };
+  // garage.showGarage = function() {
+  //   var listMaintenance = render(garage.savedCarMaintenance[0][1][2][3][4][6][0][0]);
+  //   console.log(garage.savedCarMaintenance.length);
+  //   $('#maintenance').append(listMaintenance);
+  // }; // TODO : COULD BE USED TO MAKE BOTH CALLS AT ONCE AND/OR JUST MAKE A "MORE INFO" TAG TO EXTEND MAINT INFO
+
   module.garage = garage;
 })(window);
