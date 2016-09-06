2b10e4f143fed5c2184df4da432b6b8bc2cd468e
Kyle Winckler <kyle.winckler@gmail.com>
2016-09-05 20:46:45 -0700

    database error

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('/user', function (req, res) {
   var client = new pg.Client(process.env.DATABASE_URL);
   client.connect(function(err) {
     if (err) throw err;
-    client.query('INSERT INTO cars (email) values ($1)', [req.user._json.email], function(err, result) {
+    client.query('INSERT INTO garage (email) values ($1)', [req.user._json.email], function(err, result) {
       if (err) throw err;
       console.log(result);
       client.end(function(err) {
