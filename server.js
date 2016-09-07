8b80c75cbaf071c41b67cfc21d37981e7c08bbe6
Kyle Winckler <kyle.winckler@gmail.com>
2016-09-06 21:15:09 -0700

    comment out section breaking the page

--- server.js
+++ server.js
@@ -60,17 +60,17 @@ app.get('/user', function (req, res) {
 app.get('/addCar', function(req, res) {
   console.log(req.query.currentCar);
   console.log(req.query.email);
-  var data = {car: req.query.currentCar, email: req.query.email};
-  var client = new pg.Client(process.env.DATABASE_URL);
-  client.connect(function(err) {
-    if (err) throw err;
-    client.query('UPDATE garage SET cars = cars || ($1) WHERE email=($2)',[data.car, data.email] function(err, result) {
-      if (err) throw err;
-      client.end(function(err) {
-        if (err) throw err;
-      });
-    });
-  });
+  // var data = {car: req.query.currentCar, email: req.query.email};
+  // var client = new pg.Client(process.env.DATABASE_URL);
+  // client.connect(function(err) {
+  //   if (err) throw err;
+  //   client.query('UPDATE garage SET cars = cars || ($1) WHERE email=($2)',[data.car, data.email] function(err, result) {
+  //     if (err) throw err;
+  //     client.end(function(err) {
+  //       if (err) throw err;
+  //     });
+  //   });
+  // });
 });
 
 /////////////////////////////////////////////////////////////////////////////////////////////
