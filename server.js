d9a106849eac01b79156cf9c27719e6ac115f205
Kyle Winckler <kyle.winckler@gmail.com>
2016-09-06 23:12:57 -0700

    type test

--- server.js
+++ server.js
@@ -58,8 +58,8 @@ app.get('/user', function (req, res) {
 });
 
 app.get('/addCar', function(req, res) {
-  console.log(req.query.currentCar);
-  console.log(req.query.email);
+  console.log(typeOf(req.query.currentCar));
+  console.log(typeOf(req.query.email));
   // var data = {car: req.query.currentCar, email: req.query.email};
   var client = new pg.Client(process.env.DATABASE_URL);
   client.connect(function(err) {
