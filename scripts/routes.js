da991d32db9dc39ae28b345352790d776c6e6653
codyhulsey <sendcodyamessage@live.com>
2016-09-06 21:12:41 -0700

    Calendar is now populated. Also set up routes and page.

--- scripts/routes.js
+++ scripts/routes.js
@@ -2,6 +2,8 @@ page('/', loginController.init, loginView.show);
 page('/garage', garageController.init, garageView.show);
 page('/search', searchTool.init, searchView.show);
 page('/addCar', searchView.addCar);
+page('/calendar', calendarController.init, calendarView.show);
+
 
 
 page();
