50c342f011d0d0b72278e566242e34574606364f
Kyle Winckler <kyle.winckler@gmail.com>
2016-09-06 21:09:37 -0700

    possible fix

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/addCar', function(req, res) {
   var client = new pg.Client(process.env.DATABASE_URL);
   client.connect(function(err) {
     if (err) throw err;
-    client.query('UPDATE garage SET cars = cars || car={($1)} WHERE email=($2)',[data.car, data.email] function(err, result) {
+    client.query('UPDATE garage SET cars = cars || ($1) WHERE email=($2)',[data.car, data.email] function(err, result) {
       if (err) throw err;
       client.end(function(err) {
         if (err) throw err;
