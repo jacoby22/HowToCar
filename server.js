3d92071128d7fe1bd654184419ff09ff954a5d32
Kyle Winckler <kyle.winckler@gmail.com>
2016-09-08 23:09:19 -0700

    another bug fix

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get('/user', function (req, res) {
 });
 
 app.get('/addCar', function(req, res) {
-  var formattedCar = '{' + req.query.currentCar.make + '/' + req.query.currentCar.model + '/' + req.query.currentCar.year '/' + req.query.currentCar.id + '}';
+  var formattedCar = '{' + req.query.currentCar.make + '/' + req.query.currentCar.model + '/' + req.query.currentCar.year + '/' + req.query.currentCar.id + '}';
   var client = new pg.Client(process.env.DATABASE_URL);
   client.connect(function(err) {
     if (err) throw err;
