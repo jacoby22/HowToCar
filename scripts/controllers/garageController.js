ecd2337990245bcf3122b71f4def3f41e89137d6
Kyle Winckler <kyle.winckler@gmail.com>
2016-09-08 17:49:12 -0700

    add callback

--- scripts/controllers/garageController.js
+++ scripts/controllers/garageController.js
@@ -2,7 +2,7 @@
 
   var garageController = {};
 
-  garageController.init = function(ctx, next) {
+  garageController.init = function(ctx, next, callback) {
     garage.showCar(callback);   //  TODO :THIS CALL ADDS ADDNL DATA EVERYTIME Garage Link is clicked.
     next();
   };
