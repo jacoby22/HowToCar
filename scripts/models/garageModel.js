434dc696039dd1b7597e9ceee70372ed6005372b
Kyle Winckler <kyle.winckler@gmail.com>
2016-09-08 19:16:35 -0700

    url debug

--- scripts/models/garageModel.js
+++ scripts/models/garageModel.js
@@ -17,7 +17,7 @@
   };
 
   garage.getCarMaintenance = function(callback, carId) {
-    $.get('/maintenance/actionrepository/findbymodelyearid', {modelyearid: carId});
+    $.get('/maintenance/actionrepository/findbymodelyearid');
     // .done(function(data) {
     //   callback(data);
     // });
