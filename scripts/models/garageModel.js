2e2679cc590a12b081aa5c02bde9246beb1e573f
Nick Jacoby <nickjacoby22@gmail.com>
2016-09-09 21:12:12 -0700

    balls

--- scripts/models/garageModel.js
+++ scripts/models/garageModel.js
@@ -43,7 +43,7 @@
       $('#car').append(listItem);
       currentCar.maintenance.forEach(function(maintItem) {
         var maintElem = renderMaintenace(maintItem);
-        $('#' + currentCar.content + '').append(maintElem);
+        $('#car-template').append(maintElem);
       });
     });
     //   var listMaintenance = renderMaintenace(data);
