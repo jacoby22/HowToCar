e732513b1d1196639e34bc1924d3a8ac0f82e158
Nick Jacoby <nickjacoby22@gmail.com>
2016-09-09 19:45:30 -0700

    balls

--- scripts/models/garageModel.js
+++ scripts/models/garageModel.js
@@ -1,5 +1,12 @@
 (function(module) {
 
+  Handlebars.registerHelper('times', function(n, block) {
+    var accum = '';
+    for(var i = 0; i < n; ++i)
+      accum += block.fn(i);
+    return accum;
+  });
+
   var garage = {};
 
   function ParkedCar(data, splitCar, car) {
