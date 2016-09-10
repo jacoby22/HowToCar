6d5952b144e981f4eebca88bc041752a87278144
Kyle Winckler <kyle.winckler@gmail.com>
2016-09-10 12:23:13 -0700

    remove element on click

--- scripts/controllers/garageController.js
+++ scripts/controllers/garageController.js
@@ -3,7 +3,7 @@
   var garageController = {};
 
   garageController.init = function(ctx, next, callback) {
-    garage.showCar(garage.deleteFromDb(tempArray));   //  TODO :THIS CALL ADDS ADDNL DATA EVERYTIME Garage Link is clicked.
+    garage.showCar();   //  TODO :THIS CALL ADDS ADDNL DATA EVERYTIME Garage Link is clicked.
     next();
   };
 
